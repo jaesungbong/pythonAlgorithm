3866a693d66161040d5392c677100fcd77b99b79
jaesungbong <jaesungbong@gmail.com>
2025-03-10 22:51:27 +0900

    20250310 수정

--- 재귀_codeup_우박수 길이 (3n+1) (large).py
+++ 재귀_codeup_우박수 길이 (3n+1) (large).py
@@ -1,19 +1,34 @@
-def recur(n, cnt):
-    cnt += 1
+def recur(n):
     if n == 1:
-        return cnt
+        return 1
+
     k = 0
-    if n % 2 != 0:
-        k = 3 * n + 1
-    else:
+
+    if arr[n] != 0:
+        return arr[n]
+
+    if n % 2 == 0:
         k = n // 2
-    return recur(k, cnt)
+    else:
+        k = 3 * n + 1
+    arr[n] = recur(k) + 1
 
 start, end = map(int, input().split())
-#
-# num = 0
-# max = float('-inf')
-#
-# for i in range(start, end + 1):
-#     cnt = 0
-#     recur(i)
+
+max_num = 0
+max_val = float('-inf')
+
+arr = [0] * (end + 1)
+arr[1] = 1
+
+for i in range(start, end + 1):
+    if arr[i] != 0:
+        recur(i)
+    print(i, arr[i])
+    # k = recur(i)
+    # print(i, k)
+    # if k > max_val:
+    #     max_val = k
+    #     max_num = i
+
+# print(max_num, max_val)
