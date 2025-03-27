ff67c441535ac584ee8ab8d63fbf4b5a0f41f2b8
e210293 <manle4@hanwha.com>
2025-03-27 17:12:44 +0900

    20250327 업데이트

--- 탐색기반_codeup_2641_숏다리의 계단 오르기 (Small)_재귀.py
+++ 탐색기반_codeup_2641_숏다리의 계단 오르기 (Small)_재귀.py
@@ -1,16 +1,20 @@
-def stair_climb(arr, n):
-    if n == 1:
-        arr[1] = 1
-    elif n == 2:
-        arr[2] = 2
-    elif n == 3:
-        arr[3] = 4
+def count_ways(target, numbers):
+    if target == 0:
+        return 1  # 합이 0이 되는 경우는 1가지 (아무것도 선택하지 않음)
+    if target < 0:
+        return 0  # 합이 음수가 되는 경우는 0가지
 
-    arr[n] = arr[n - 2] + arr[n - 1]
+    total_ways = 0
+    for number in numbers:
+        total_ways += count_ways(target - number, numbers)
+
+    return total_ways
 
 
-n = int(input()) #계단 목표
-arr = [0] * (n + 1) #계단 오르기 경우의 수
-arr = [0] * (n + 1) #계단 오르기 경우의 수
-#print(stair_climb(arr, n))
+# 원소 리스트
+numbers = [1, 2, 3]
+target_sum = 5
 
+# 경우의 수 계산
+result = count_ways(target_sum, numbers)
+print(result)
