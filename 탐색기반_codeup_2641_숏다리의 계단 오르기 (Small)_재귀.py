4aea7cf4a48513d9cef815341bc024aa2610ca57
jaesungbong <jaesungbong@gmail.com>
2025-03-28 22:08:19 +0900

    20250328 수정

--- 탐색기반_codeup_2641_숏다리의 계단 오르기 (Small)_재귀.py
+++ 탐색기반_codeup_2641_숏다리의 계단 오르기 (Small)_재귀.py
@@ -1,20 +1,24 @@
-def count_ways(target, numbers):
+def count_ways(target, cnt):
+    if cnt > 0:
+        cnt = cnt - 1
+
     if target == 0:
         return 1  # 합이 0이 되는 경우는 1가지 (아무것도 선택하지 않음)
     if target < 0:
         return 0  # 합이 음수가 되는 경우는 0가지
 
     total_ways = 0
-    for number in numbers:
-        total_ways += count_ways(target - number, numbers)
 
+    total_ways += count_ways(target - 1, cnt)
+    total_ways += count_ways(target - 2, cnt)
+    if cnt == 0:
+        total_ways += count_ways(target - 3, 3)
     return total_ways
 
 
 # 원소 리스트
-numbers = [1, 2, 3]
-target_sum = 5
+target_sum = int(input())
 
 # 경우의 수 계산
-result = count_ways(target_sum, numbers)
+result = count_ways(target_sum, 0)
 print(result)
