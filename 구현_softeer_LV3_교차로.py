e8023d917e6aa890768de903a49c2ffaac95417f
e210293 <manle4@hanwha.com>
2025-04-08 16:57:36 +0900

    20250408 업데이트

--- 구현_softeer_LV3_교차로.py
+++ 구현_softeer_LV3_교차로.py
@@ -1,58 +1,59 @@
-def delay_time(arr, primary_key):
-    for i in range(len(arr)):
-        if arr[i][1] == primary_key:
-            arr[i][0] += 1
-    return arr
-
-
-def time_pass(arr, time):
-    temp_list = [item[1] for item in arr if item[0] == min_time]
-    for j in temp_list:
-        if j == "A":
-            if "D" in temp_list:
-                delay_time(arr, "A")
+def delay_time(high_way, primary_key, time):
+    for i in range(len(high_way)):
+        if high_way[i][1] == primary_key:
+            if high_way[i][0] == time:
+                high_way[i][0] += 1
+                time += 1
             else:
-                print(time)  # 지나가는 시간 출력
-        elif j == "B":
-            if "A" in temp_list:
-                delay_time(arr, "B")
-            else:
-                print(time)  # 지나가는 시간 출력
-        elif j == "C":
-            if "B" in temp_list:
-                delay_time(arr, "C")
-            else:
-                print(time)  # 지나가는 시간 출력
-        elif j == "D":
-            if "C" in temp_list:
-                delay_time(arr, "D")
-            else:
-                print(time)  # 지나가는 시간 출력
-    print(arr)
-
+                break
+    return high_way
 
 n = int(input())
 
 min_time = 1000000001
-max_time = -1
 high_way = []
 
 for i in range(n):
     time, way = input().split()
     high_way.append([int(time), way])
-    max_time = max((int(time), max_time))
     min_time = min((int(time), min_time))
 
 # D > A > B > C
+count = 0
 
-while True:
-    if min_time == max_time:
-        break
-    time_pass(high_way, min_time)
+while count != n:
+    temp_list = [item[1] for item in high_way if item[0] == min_time]
+    if "A" in temp_list and "B" in temp_list and "C" in temp_list and "D" in temp_list:
+        high_way[high_way.index([min_time, "A"])][0] = -1
+        high_way[high_way.index([min_time, "B"])][0] = -1
+        high_way[high_way.index([min_time, "C"])][0] = -1
+        high_way[high_way.index([min_time, "D"])][0] = -1
+        count += 4
+    else:
+        for j in temp_list:
+            if j == "A":
+                if "D" in temp_list:
+                    delay_time(high_way, "A", min_time)
+                else:
+                    count += 1
+            elif j == "B":
+                if "A" in temp_list:
+                    delay_time(high_way, "B", min_time)
+                else:
+                    count += 1
+            elif j == "C":
+                if "B" in temp_list:
+                    delay_time(high_way, "C", min_time)
+                else:
+                    count += 1
+            elif j == "D":
+                if "C" in temp_list:
+                    delay_time(high_way, "D", min_time)
+                else:
+                    count += 1
     min_time += 1
-    max_time = max([itm[0] for itm in high_way])
 
-
-
+for i in high_way:
+    print(i[0])
 
 
