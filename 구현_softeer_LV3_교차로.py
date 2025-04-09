fee08c1b5202b18291282969dea263e21fd6b9d6
e210293 <manle4@hanwha.com>
2025-04-09 16:41:41 +0900

    20250409 업데이트

--- 구현_softeer_LV3_교차로.py
+++ 구현_softeer_LV3_교차로.py
@@ -5,7 +5,7 @@ n = int(input())
 
 way = ["A", "B", "C", "D"]
 ways = [deque() for _ in range(4)]
-left_way = [1, 2, 3, 0] # A일때 B, B일때 C, C일때 D, D일때 A
+right_way = [3, 0, 1, 2]
 min_time = 1000000001
 
 for i in range(n):
@@ -20,23 +20,33 @@ print(ways)
 result = []
 
 while ways[0] or ways[1] or ways[2] or ways[3]:
+    delay_list = []
+
     for i in range(len(ways)):
         if ways[i] and min_time == ways[i][0]:
-            # 왼쪽에 차량이 있다면 왼쪽과 그 뒤 이어 지는 차량은 모두 +1 해준다.
-            if ways[left_way[i]] and ways[left_way[i]][0] == min_time:
-                temp_time = min_time
-                for j in range(len(ways[left_way[i]])):
-                    if ways[left_way[i]][j] == temp_time:
-                        ways[left_way[i]][j] += 1
-                        temp_time += 1
-                    else:
-                        break
+            # 오른쪽에 차량이 있다면 자신과 그 바로 뒤 이어 지는 차량은 모두 +1 해준다.
+            if ways[right_way[i]] and ways[right_way[i]][0] == min_time:
+                delay_list.append([i, 0])
+
+    for item in delay_list:
+        temp_time = min_time
+        time = item[1]
+        for j in range(len(ways[item[0]])):
+            if ways[item[0]][j] == temp_time:
+                ways[item[0]][j] += 1
+                temp_time += 1
+            else:
+                break
+
+    for i in range(len(ways)):
+        if ways[i] and min_time == ways[i][0]:
             result.append(ways[i].popleft())
-            min_time += 1
+
+    min_time += 1
     print("A:{}, B:{}, C:{}, D:{}".format(ways[0], ways[1], ways[2], ways[3]))
     print()
-print(result)
 
+print(result)
 
 
 
